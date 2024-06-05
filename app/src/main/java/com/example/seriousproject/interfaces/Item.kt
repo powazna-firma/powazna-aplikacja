6042bab8cbd98f4791333669b854c47a36b725fa
Adwin120 <adam.winnik120@gmail.com>
2024-06-05 22:18:09 +0200

    fix Item not being serializable

--- app/src/main/java/com/example/seriousproject/interfaces/Item.kt
+++ app/src/main/java/com/example/seriousproject/interfaces/Item.kt
@@ -1,10 +1,11 @@
 package com.example.seriousproject.interfaces
 
 import androidx.compose.ui.graphics.painter.Painter
+import java.io.Serializable
 
 data class Item(
     val title: Int,
     val image: Int,
     val description: Int,
     val price: Float,
-)
+) : Serializable
